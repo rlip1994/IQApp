8d00da61cb384422a7d8528e5e427a3fc4bc1549
rlip1994 <solisbarrantes@gmail.com>
2017-08-28 13:09:16 -0600

    configuracion del package.json y refactor general

--- iqweb/app/app.js
+++ iqweb/app/app.js
@@ -1,14 +1,33 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('smart', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+ 
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider
+
+  .when('/login',{
+    controller: 'LoginController',
+    templateUrl: 'login/view.html',
+    controllerAs: 'vm'
+  })
+  
+  .when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl',
+    controllerAs: 'vm'
+  })
+
+  .when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl',
+    controllerAs: 'vm'
+  })
+
+  
+  .otherwise({redirectTo: '/view1'});
 }]);
